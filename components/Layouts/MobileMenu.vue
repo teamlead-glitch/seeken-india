0220e55245a5262a6e721f3957092b4452ef9870
Sanil K <tech@phitany.com>
2025-03-04 12:50:14 +0530

    case changes

---
+++ components/Layouts/MobileMenu.vue
@@ -0,0 +1,29 @@
+<template>
+<!-- mobile menu -->
+<div class="mobile_menu">
+    <div class="mob__menu">
+      <div class="nav"><a href="#">Home</a></div>
+      <div class="logo"><a href="index.html"><img src="/images/logo-blue.svg" alt="logo"></a>  </div>
+      <div class="search_wishlist">
+        <!-- <div class="wishlist"> <a href="wishlist.html">
+            <div class="icon"><img src="/images/whislist.svg" alt="search">
+              <div class="count">2</div>
+            </div>
+          </a> </div> -->
+        <div class="search">
+          <div class="icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTopone"
+            aria-controls="offcanvasTop"><img src="/images/search_icon.svg" alt="search"></div>
+        </div>
+
+      </div>
+    </div>
+    <div class="grab_sale">
+      <div class="close-btns">
+        <i class="bi bi-chevron-down"></i>
+      </div>
+      <h6>Grab your Flash Deals Now</h6>
+      <p style="display: none;">We have a flash sale running with 40-70% off for a limited period and stock.</p>
+    </div>
+  </div>
+  <!-- mobile menu close -->
+</template>
