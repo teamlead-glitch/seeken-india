e1bacf8d15377ac02103815d2d630b6de8cf6bba
Sherin Reynold <teamlead@phitany.com>
2025-03-19 19:49:00 +0530

    latest article mobile/desk component switching

--- components/LatestArticles.vue
+++ components/LatestArticles.vue
@@ -1,16 +1,18 @@
 <template>
-    <LatestArticlesDesk :articles="articles"/>
-    <!-- <LatestArticlesMob :articles="articles"/> -->
+    <LatestArticlesDesk v-if="!isMobile" :articles="articles"/>
+    <LatestArticlesMob v-if="isMobile" :articles="articles"/>
 </template>
 
 <script setup>
 
 import { useRouter } from 'vue-router';
+import { useMediaQuery } from '@vueuse/core';
 
 const router = useRouter();
 const config = useRuntimeConfig();
 
-
+const isMobile = useMediaQuery('(max-width: 768px)');
+console.log(isMobile.value,'ismob')
 
 
 // Fetch slide data during SSR
--- components/LatestArticlesMob.vue
+++ components/LatestArticlesMob.vue
@@ -3,7 +3,7 @@
   <section class="latest__articles__mobile">
     <div class="container">
       <div class="row align-items-center">
-        <div class="col-xl-6 col-md-8  mb-3">
+        <div class="col-xl-6 col-md-8 mb-3">
           <h3>Latest Articles</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis aliquet metus, eget ullamcorper nisi
             egestas vitae.</p>
@@ -11,126 +11,37 @@
       </div>
       <div class="row mt-3">
         <div class="col-md-12">
-          <div class="swiper mySwiper3">
-            <div class="swiper-wrapper">
-              <div class="swiper-slide">
-                <a href="#">
-                  <div class="img__box">
-                    <img src="/images/blog-1.webp" class="img-fluid" alt="seeken" loading="lazy">
-                  </div>
-                  <div class="content">
-                    <div class="calendar"><i class="bi bi-calendar4"></i> February 11, 2025</div>
-                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi
-                      quisquam,
-                      cum similique debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem
-                      quis
-                      quos nostrum exercitationem sequi.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis aliquet metus, eget
-                      ullamcorper
-                      nisi egestas vitae. Donec scelerisque pharetra vulputate. In quis purus vitae est fringilla
-                      ultricies.
-                      sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi quisquam, cum similique
-                      debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem quis quos nostrum
-                      exercitationem sequi.</p>
-                  </div>
-                </a>
-              </div>
-              <div class="swiper-slide">
-                <a href="#">
-                  <div class="img__box">
-                    <img src="/images/blog-2.webp" class="img-fluid" alt="seeken" loading="lazy">
-                  </div>
-                  <div class="content">
-                    <div class="calendar"><i class="bi bi-calendar4"></i> February 03, 2025</div>
-                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi
-                      quisquam,
-                      cum similique debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem
-                      quis
-                      quos nostrum exercitationem sequi.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis aliquet metus, eget
-                      ullamcorper
-                      nisi egestas vitae. Donec scelerisque pharetra vulputate. In quis purus vitae est fringilla
-                      ultricies.
-                      sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi quisquam, cum similique
-                      debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem quis quos nostrum
-                      exercitationem sequi.</p>
-                  </div>
-                </a>
-              </div>
-              <div class="swiper-slide">
-                <a href="#">
-                  <div class="img__box">
-                    <img src="/images/blog-3.webp" class="img-fluid" alt="seeken" loading="lazy">
-                  </div>
-                  <div class="content">
-                    <div class="calendar"><i class="bi bi-calendar4"></i> January 23, 2025</div>
-                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi
-                      quisquam,
-                      cum similique debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem
-                      quis
-                      quos nostrum exercitationem sequi.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis aliquet metus, eget
-                      ullamcorper
-                      nisi egestas vitae. Donec scelerisque pharetra vulputate. In quis purus vitae est fringilla
-                      ultricies.
-                      sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi quisquam, cum similique
-                      debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem quis quos nostrum
-                      exercitationem sequi.</p>
-                  </div>
-                </a>
-              </div>
-              <div class="swiper-slide">
-                <a href="#">
-                  <div class="img__box">
-                    <img src="/images/blog-2.webp" class="img-fluid" alt="seeken" loading="lazy">
-                  </div>
-                  <div class="content">
-                    <div class="calendar"><i class="bi bi-calendar4"></i> February 03, 2025</div>
-                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi
-                      quisquam,
-                      cum similique debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem
-                      quis
-                      quos nostrum exercitationem sequi.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis aliquet metus, eget
-                      ullamcorper
-                      nisi egestas vitae. Donec scelerisque pharetra vulputate. In quis purus vitae est fringilla
-                      ultricies.
-                      sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi quisquam, cum similique
-                      debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem quis quos nostrum
-                      exercitationem sequi.</p>
-                  </div>
-                </a>
-              </div>
-              <div class="swiper-slide">
-                <a href="#">
-                  <div class="img__box">
-                    <img src="/images/blog-3.webp" class="img-fluid" alt="seeken" loading="lazy">
-                  </div>
-                  <div class="content">
-                    <div class="calendar"><i class="bi bi-calendar4"></i> January 23, 2025</div>
-                    <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi
-                      quisquam,
-                      cum similique debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem
-                      quis
-                      quos nostrum exercitationem sequi.</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis aliquet metus, eget
-                      ullamcorper
-                      nisi egestas vitae. Donec scelerisque pharetra vulputate. In quis purus vitae est fringilla
-                      ultricies.
-                      sit amet consectetur adipisicing elit. Possimus voluptas quibusdam quasi quisquam, cum similique
-                      debitis, odio, nihil alias eveniet harum. Corrupti consectetur provident dolorem quis quos nostrum
-                      exercitationem sequi.</p>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div class="swiper-button-prev">
-              <i class="bi bi-arrow-left"></i>
-            </div>
-            <div class="swiper-button-next">
-              <i class="bi bi-arrow-right"></i>
-            </div>
-          </div>
+          <Swiper
+          :modules="[Autoplay, Navigation, Pagination]"
+          :spaceBetween="30"
+          :slidesPerView="4"
+          :centeredSlides="false"
+          :loop="true"
+          :autoplay="{ delay: 4000, disableOnInteraction: false }"
+          :navigation="{ nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' }"
+          :breakpoints="{
+              1024: { slidesPerView: 4, spaceBetween: 30 },
+              768: { slidesPerView: 3, spaceBetween: 20 },
+              480: { slidesPerView: 1.5, spaceBetween: 15 },
+              380: { slidesPerView: 1, spaceBetween: 10 }
+            }"
+          class="mySwiper"
+          >
+            <SwiperSlide v-for="(item, index) in articles" :key="index" class="swiper-slide">
+              <a href="#">
+                <div class="img__box">
+                  <img :src="item.image_path" class="img-fluid" alt="seeken" loading="lazy">
+                </div>
+                <div class="content">
+                  <div class="calendar"><i class="bi bi-calendar4"></i> {{ formatDate(item.created_at) }}</div>
+                  <h3>{{ item.title }}</h3>
+                  <p>{{ item.description }}</p>
+                </div>
+              </a>
+            </SwiperSlide>
+            <div class="swiper-button-prev"><i class="bi bi-arrow-left"></i></div>
+            <div class="swiper-button-next"><i class="bi bi-arrow-right"></i></div>
+          </Swiper>
         </div>
       </div>
       <div class="row">
@@ -141,3 +52,70 @@
     </div>
   </section>
 </template>
+
+<script setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Autoplay, Navigation, Pagination } from "swiper/modules"; // Import required modules
+import { useRouter } from "vue-router";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+
+import { useDateFormat } from '~/composables/useDateFormat';
+const { formatDate } = useDateFormat();
+
+const props = defineProps({
+  articles: Array
+});
+</script>
+
+<style scoped>
+
+/* Hide default Swiper big arrows */
+.swiper-button-next::after,
+.swiper-button-prev::after {
+  display: none;
+}
+
+/* Style for the new navigation arrows */
+.swiper-button-next,
+.swiper-button-prev {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: rgba(0, 0, 0, 0.7); /* Black with transparency */
+  border-radius: 50%;
+  width: 45px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  z-index: 10;
+  transition: background-color 0.3s, transform 0.3s;
+}
+
+/* Centering the arrows */
+.swiper-button-prev {
+  left: 15px;
+}
+
+.swiper-button-next {
+  right: 15px;
+}
+
+/* Hover effect */
+.swiper-button-next:hover,
+.swiper-button-prev:hover {
+  background-color: rgba(0, 0, 0, 0.9);
+  transform: translateY(-50%) scale(1.1);
+}
+
+/* Icon style */
+.swiper-button-next i,
+.swiper-button-prev i {
+  color: white;
+  font-size: 20px;
+}
+</style>
