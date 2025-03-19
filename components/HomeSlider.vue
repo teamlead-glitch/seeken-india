211327cd96566ba0190e75d122859962e857c956
Sherin Reynold <teamlead@phitany.com>
2025-03-19 20:58:13 +0530

    home slider static json list

--- components/HomeSlider.vue
+++ components/HomeSlider.vue
@@ -2,37 +2,32 @@
 <section class="products__container" id="section1">
     <div class="container-fluid p-0">
       <div class="row">
-        <div class="col-md-4 p-0">
+        
+        
+        <div class="col-md-4 p-0" v-for="(item, index) in products" :key="index">
           <div class="products__img">
-            <img src="/images/product_1.webp" class="img-fluid" alt="seeken">
+            <img :src="item.image" class="img-fluid" alt="seeken">
             <div class="contents">
-              <h6>Lorum ipsum</h6>
-              <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, </h3>
+              <h6>{{item.title}}</h6>
+              <h3>{{item.description}}</h3>
               <a class="btn_1" href="#">Buy </a>
             </div>
           </div>
         </div>
-        <div class="col-md-4 p-0">
-          <div class="products__img">
-            <img src="/images/product_2.webp" class="img-fluid" alt="seeken">
-            <div class="contents">
-              <h6>Lorum ipsum</h6>
-              <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, </h3>
-              <a class="btn_1" href="#">Buy </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 p-0">
-          <div class="products__img">
-            <img src="/images/product_3.webp" class="img-fluid" alt="seeken">
-            <div class="contents">
-              <h6>Lorum ipsum</h6>
-              <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores, </h3>
-              <a class="btn_1" href="#">Buy </a>
-            </div>
-          </div>
-        </div>
+
+
       </div>
     </div>
   </section>
 </template>
+
+<script setup>
+const products = [
+  { title: "New Prod 1", description: "This is the first slide content.", image:"/images/product_1.webp" },
+  { title: "New Prod 2", description: "This is the second slide content.", image:"/images/product_2.webp" },
+  { title: "New Prod 3", description: "This is the third slide content.", image:"/images/product_3.webp" },
+  
+];
+
+
+</script>
