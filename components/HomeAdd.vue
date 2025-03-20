0e9557964f6a7b2301be8cbba2547721ebf8699e
Sherin Reynold <teamlead@phitany.com>
2025-03-20 16:22:01 +0530

    home add with static response

---
+++ components/HomeAdd.vue
@@ -0,0 +1,10 @@
+<template>
+    <section class="ad_container">
+    <img :src="add.web_image" class="img-fluid banner__desk" alt="seeken" >
+    <img :src="add.mob_image" class="img-fluid banner__mob" alt="seeken" >
+  </section>
+</template>
+
+<script setup>
+const add = { title: "add 1" ,web_image: "/images/ad.webp", mob_image: "/images/ad-mobile.jpg"}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,12 @@
 <template>
-
   <HomeBanner />
-
   <HomeSlider />
-
   <FlashSale />
-
-
   <WhatsNewDesktop />
-
   <WhatsNewMob />
-
   <Category />
-
   <HomeFeaturedProducts />
-
-
-
-  <section class="ad_container">
-    <img src="/images/ad.webp" class="img-fluid banner__desk" alt="seeken" loading="lazy">
-    <img src="/images/ad-mobile.jpg" class="img-fluid banner__mob" alt="seeken" loading="lazy">
-  </section>
-
+  <HomeAdd />
   <TopPicks />
-
   <LatestArticles />
-
-  
-
-
-
-
-
-
-
-
 </template>
