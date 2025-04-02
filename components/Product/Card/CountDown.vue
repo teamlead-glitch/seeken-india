0a8912a99dbd72f8747db0e211a1fd895aa06666
Sherin Reynold <teamlead@phitany.com>
2025-04-02 12:24:46 +0530

    counter updates

--- components/Product/Card/CountDown.vue
+++ components/Product/Card/CountDown.vue
@@ -1,17 +1,64 @@
 <template>
     <div class="countdown-container" data-endtime="7">
                 <div class="countdown-box">
-                  <span id="days">02</span>
+                  <span id="days">{{days}}</span>
                   <span class="label">DAYS</span>
                 </div>
                 <div class="countdown-box">
-                  <span id="hours">23</span>
+                  <span id="hours">{{hours}}</span>
                   <span class="label">HOURS</span>
                 </div>
                 <div class="countdown-box">
-                  <span id="minutes">57</span>
+                  <span id="minutes">{{minutes}}</span>
                   <span class="label">MINUTES</span>
+                </div>
+                <div class="countdown-box">
+                  <span id="minutes">{{seconds}}</span>
+                  <span class="label">Seconds</span>
                 </div>
 
               </div>
+
+             
 </template>
+
+<script setup>
+
+
+import { ref, onMounted, onUnmounted, computed } from 'vue';
+  
+  const props = defineProps({
+    endTime: {
+      type: String,
+      required: true,
+    },
+  });
+
+
+  
+  const timeLeft = ref(0);
+
+
+const days = computed(() => Math.floor(timeLeft.value / 1000 / 60 / 60 / 24));
+const hours = computed(() => Math.floor((timeLeft.value / 1000 / 60 / 60) % 24));
+const minutes = computed(() => Math.floor((timeLeft.value / 1000 / 60) % 60));
+const seconds = computed(() => Math.floor((timeLeft.value / 1000) % 60));
+
+  
+  const updateTimer = () => {
+    const endTimeMs = new Date(props.endTime).getTime();
+    const nowMs = Date.now();
+    timeLeft.value = Math.max(0, endTimeMs - nowMs);
+  };
+  
+  let intervalId;
+  
+  onMounted(() => {
+    updateTimer();
+    intervalId = setInterval(updateTimer, 1000);
+  });
+  
+  onUnmounted(() => {
+    clearInterval(intervalId);
+  });
+</script>
--- components/Product/FeaturedCard.vue
+++ components/Product/FeaturedCard.vue
@@ -10,7 +10,7 @@
               </div>
               
               <ProductCardFlashSaleTag :is_flashsale="product.is_flash_sale" :item_left="product.stock_quantity" />
-              <ProductCardCountDown />
+              <ProductCardCountDown :endTime="product.to_date" v-if="product.to_date"/>
               <a data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" @click="setQuickProduct(product.slug)">
                 <div class="quick__buy">QUICK BUY</div>
               </a>
