197e12528d97e253c3e24a01b0505150797dd9b5
Sherin Reynold <teamlead@phitany.com>
2025-04-04 19:53:34 +0530

    product details page updates

---
+++ components/Product/ReviewsSection.vue
@@ -0,0 +1,189 @@
+<template>
+    
+    <div class="row justify-content-center mt-3 mt-md-5">
+        <div class="col-md-12 review_rating">
+            <h4>Customer reviews & ratings</h4>
+<div class="review_star">
+                <div class="reviewrating_boxes">
+                    <div class="rating">
+                        <h3>4.3</h3>
+                        <div class="ratings">
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill"></i>
+                            <i class="bi bi-star-fill hash"></i>
+                        </div>
+                        <div class="total__rating">
+                            46 ratings
+                        </div>
+                    </div>
+                    <div class="recommended">
+                        <h3>86%</h3>
+                        <div class="total__rating">
+                            Recommended <span>(13 of 15)</span>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="star__rating">
+                    <div class="percentage__box">
+                        <div class="stars_rating">5 Stars</div>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-label="Basic example"
+                                style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                            </div>
+                        </div>
+                        <div class="percentage_rating">48</div>
+                    </div>
+
+                    <div class="percentage__box">
+                        <div class="stars_rating">4 Stars</div>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-label="Basic example"
+                                style="width: 75%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                            </div>
+                        </div>
+                        <div class="percentage_rating">34</div>
+                    </div>
+
+                    <div class="percentage__box">
+                        <div class="stars_rating">3 Stars</div>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-label="Basic example"
+                                style="width: 50%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                            </div>
+                        </div>
+                        <div class="percentage_rating">27</div>
+                    </div>
+
+                    <div class="percentage__box">
+                        <div class="stars_rating">2 Stars</div>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-label="Basic example"
+                                style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                            </div>
+                        </div>
+                        <div class="percentage_rating">10</div>
+                    </div>
+
+                    <div class="percentage__box">
+                        <div class="stars_rating">1 Stars</div>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" aria-label="Basic example"
+                                style="width: 0%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                            </div>
+                        </div>
+                        <div class="percentage_rating">03</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row customer__review justify-content-center">
+
+        <div class="col-md-8 col-xl-6">
+            <div class="reviewrating_box ">
+                <!-- this div show only  customer didnot login -->
+                <h4>Write a review</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, animi sequi cupiditate alias doloribus eligendi dolor, commodi quaerat quo, eum ratione. Adipisci eaque tenetur pariatur vel repellat, aut dicta cumque?</p>
+                <button class="btn_2" data-bs-toggle="modal"
+                data-bs-target="#exampleModal5">Login</button>
+                <!--  -->
+                <div class="mb-1">
+                    <div class="ratings">
+<h5>Rating:</h5>
+<div class="stars"><i class="bi bi-star-fill"></i>
+<i class="bi bi-star-fill"></i>
+<i class="bi bi-star-fill"></i>
+<i class="bi bi-star-fill"></i>
+<i class="bi bi-star-fill hash"></i></div>
+                  </div>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Title: </label>
+                    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label"> Review:</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                  </div>
+                  <button class="btn_2">Submit</button>
+            </div>
+      </div>
+        </div>
+        <div class="row reviewrating_box mb10" data-masonry='{"percentPosition": true }'>
+
+           <div class="col-md-6 pe-2 ps-2">
+              <div class="customer_detail">
+            <h5>Title here</h5>
+            <div class="stars"><i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill"></i>
+                <i class="bi bi-star-fill hash"></i></div>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur voluptas tempore harum quaerat iure. Officiis recusandae porro similique numquam incidunt minus odio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque vel placeat,  consectetur nostrum architecto enim aperiam nesciunt perferendis maxime ipsam consequuntur unde, temporibus cumque tempore!</p>
+                <h6>Customer Name</h6>
+        </div></div>
+        <div class="col-md-6 pe-2 ps-2">
+            <div class="customer_detail">
+                <h5>Title here</h5>
+                <div class="stars"><i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill hash"></i></div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur voluptas tempore.</p>
+                    <h6>Customer Name</h6>
+            </div>
+           </div>
+           <div class="col-md-6 pe-2 ps-2">
+            <div class="customer_detail">
+                <h5>Title here</h5>
+                <div class="stars"><i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill hash"></i></div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur .</p>
+                    <h6>Customer Name</h6>
+            </div>
+           </div>
+           <div class="col-md-6 pe-2 ps-2">
+            <div class="customer_detail">
+                <h5>Title here</h5>
+                <div class="stars"><i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill hash"></i></div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur voluptas tempore harum quaerat iure. Officiis recusandae porro similique numquam incidunt minus odio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet sunt voluptatum repellat atque earum ducimus nam perferendis. Veritatis ducimus obcaecati quisquam harum porro sapiente eos.</p>
+                    <h6>Customer Name</h6>
+            </div>
+           </div>
+           <div class="col-md-6 pe-2 ps-2">
+            <div class="customer_detail">
+                <h5>Title here</h5>
+                <div class="stars"><i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill hash"></i></div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur  odio.</p>
+                    <h6>Customer Name</h6>
+            </div>
+           </div>
+           <div class="col-md-6 pe-2 ps-2">
+            <div class="customer_detail">
+                <h5>Title here</h5>
+                <div class="stars"><i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill"></i>
+                    <i class="bi bi-star-fill hash"></i></div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur voluptas tempore harum quaerat iure. Officiis recusandae porro similique numquam incidunt minus odio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium reprehenderit optio quas fugiat consequatur quaerat pariatur voluptate consequuntur velit itaque? Error fuga dolores rerum harum placeat ex excepturi necessitatibus animi?</p>
+                    <h6>Customer Name</h6>
+            </div>
+           </div>
+    </div>
+</template>
--- pages/products/[slug].vue
+++ pages/products/[slug].vue
@@ -66,196 +66,39 @@
 
         <ProductRelatedSlider :products="featured_products"/>
 
-       
-        <div class="row justify-content-center mt-3 mt-md-5">
-            <div class="col-md-12 review_rating">
-                <h4>Customer reviews & ratings</h4>
-<div class="review_star">
-                    <div class="reviewrating_boxes">
-                        <div class="rating">
-                            <h3>4.3</h3>
-                            <div class="ratings">
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill"></i>
-                                <i class="bi bi-star-fill hash"></i>
-                            </div>
-                            <div class="total__rating">
-                                46 ratings
-                            </div>
-                        </div>
-                        <div class="recommended">
-                            <h3>86%</h3>
-                            <div class="total__rating">
-                                Recommended <span>(13 of 15)</span>
+       <ProductReviewsSection/>
+</div>
 
-                            </div>
-                        </div>
-                    </div>
-                    <div class="star__rating">
-                        <div class="percentage__box">
-                            <div class="stars_rating">5 Stars</div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-label="Basic example"
-                                    style="width: 100%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <div class="percentage_rating">48</div>
-                        </div>
-
-                        <div class="percentage__box">
-                            <div class="stars_rating">4 Stars</div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-label="Basic example"
-                                    style="width: 75%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <div class="percentage_rating">34</div>
-                        </div>
-
-                        <div class="percentage__box">
-                            <div class="stars_rating">3 Stars</div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-label="Basic example"
-                                    style="width: 50%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <div class="percentage_rating">27</div>
-                        </div>
-
-                        <div class="percentage__box">
-                            <div class="stars_rating">2 Stars</div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-label="Basic example"
-                                    style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <div class="percentage_rating">10</div>
-                        </div>
-
-                        <div class="percentage__box">
-                            <div class="stars_rating">1 Stars</div>
-                            <div class="progress">
-                                <div class="progress-bar" role="progressbar" aria-label="Basic example"
-                                    style="width: 0%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                            <div class="percentage_rating">03</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row customer__review justify-content-center">
-
-            <div class="col-md-8 col-xl-6">
-                <div class="reviewrating_box ">
-                    <!-- this div show only  customer didnot login -->
-                    <h4>Write a review</h4>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, animi sequi cupiditate alias doloribus eligendi dolor, commodi quaerat quo, eum ratione. Adipisci eaque tenetur pariatur vel repellat, aut dicta cumque?</p>
-                    <button class="btn_2" data-bs-toggle="modal"
-                    data-bs-target="#exampleModal5">Login</button>
-                    <!--  -->
-                    <div class="mb-1">
-                        <div class="ratings">
-<h5>Rating:</h5>
-<div class="stars"><i class="bi bi-star-fill"></i>
-<i class="bi bi-star-fill"></i>
-<i class="bi bi-star-fill"></i>
-<i class="bi bi-star-fill"></i>
-<i class="bi bi-star-fill hash"></i></div>
-                      </div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Title: </label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
-                      </div>
-                      <div class="mb-3">
-                        <label for="exampleFormControlTextarea1" class="form-label"> Review:</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-                      </div>
-                      <button class="btn_2">Submit</button>
-                </div>
-          </div>
-            </div>
-            <div class="row reviewrating_box mb10" data-masonry='{"percentPosition": true }'>
-
-               <div class="col-md-6 pe-2 ps-2">
-                  <div class="customer_detail">
-                <h5>Title here</h5>
-                <div class="stars"><i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill"></i>
-                    <i class="bi bi-star-fill hash"></i></div>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur voluptas tempore harum quaerat iure. Officiis recusandae porro similique numquam incidunt minus odio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque vel placeat,  consectetur nostrum architecto enim aperiam nesciunt perferendis maxime ipsam consequuntur unde, temporibus cumque tempore!</p>
-                    <h6>Customer Name</h6>
-            </div></div>
-            <div class="col-md-6 pe-2 ps-2">
-                <div class="customer_detail">
-                    <h5>Title here</h5>
-                    <div class="stars"><i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill hash"></i></div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur voluptas tempore.</p>
-                        <h6>Customer Name</h6>
-                </div>
+<div class="fixed_position_rates">
+  <div class="container">
+   <div class="row">
+       <div class="col-md-12">
+           <div class="full__container">
+               <div class="product__name">
+                <h5>{{product.name}}   <span>|</span>   {{product.category_name}}</h5>
+                <div class="prices"><span>Rs. {{ product?.price }}</span>{{ product?.final_price }} </div>
                </div>
-               <div class="col-md-6 pe-2 ps-2">
-                <div class="customer_detail">
-                    <h5>Title here</h5>
-                    <div class="stars"><i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill hash"></i></div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur .</p>
-                        <h6>Customer Name</h6>
-                </div>
+               <div class="total__rate">
+                   <div class="quantity">
+                       <h4>Quatity</h4>
+                       <div class="quantity-input">
+                           <button class="quantity-btn minus-btn" @click="quantity > 1 ? quantity-- : null"><i class="bi bi-dash-lg"></i></button>
+                           <input type="number" class="quantity" v-model="quantity" min="1" max="10">
+                           <button class="quantity-btn plus-btn" @click="quantity++"><i class="bi bi-plus-lg"></i></button>
+                       </div>
+                   </div>
+                   <button class="btn_1">Add to Cart</button>
+                   <button class="btn_2">Buy Now</button>
                </div>
-               <div class="col-md-6 pe-2 ps-2">
-                <div class="customer_detail">
-                    <h5>Title here</h5>
-                    <div class="stars"><i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill hash"></i></div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur voluptas tempore harum quaerat iure. Officiis recusandae porro similique numquam incidunt minus odio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet sunt voluptatum repellat atque earum ducimus nam perferendis. Veritatis ducimus obcaecati quisquam harum porro sapiente eos.</p>
-                        <h6>Customer Name</h6>
-                </div>
-               </div>
-               <div class="col-md-6 pe-2 ps-2">
-                <div class="customer_detail">
-                    <h5>Title here</h5>
-                    <div class="stars"><i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill hash"></i></div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur  odio.</p>
-                        <h6>Customer Name</h6>
-                </div>
-               </div>
-               <div class="col-md-6 pe-2 ps-2">
-                <div class="customer_detail">
-                    <h5>Title here</h5>
-                    <div class="stars"><i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill"></i>
-                        <i class="bi bi-star-fill hash"></i></div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis nostrum est molestias non reiciendis itaque! Sint tenetur voluptas tempore harum quaerat iure. Officiis recusandae porro similique numquam incidunt minus odio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium reprehenderit optio quas fugiat consequatur quaerat pariatur voluptate consequuntur velit itaque? Error fuga dolores rerum harum placeat ex excepturi necessitatibus animi?</p>
-                        <h6>Customer Name</h6>
-                </div>
-               </div>
-        </div>
+           </div>
+       </div>
+   </div>
+  </div> 
 </div>
 
 </section>
+
+
 </template>
 
 <script lang="ts" setup>
@@ -268,6 +111,7 @@ const route = useRoute();
 const slug = route.params.slug; // Get slug from URL
 const { data: response, error, refresh } = useFetchData('response', `products/${slug}`);
 const product = computed(() => response.value?.data);
+const quantity = ref(1);
 
 const { data: featured_products, error1, refresh1 } = useFetchData('featured_products', 'featured-products');
 
