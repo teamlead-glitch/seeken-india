f76ec8d493936141cbff97b1cbfa669c33b98e5e
Sherin Reynold <teamlead@phitany.com>
2025-04-05 18:32:15 +0530

    Home flash sale mobile issue

--- components/Home/FlashSaleSlider.vue
+++ components/Home/FlashSaleSlider.vue
@@ -55,43 +55,26 @@
     
     <div class="mobile__parent__div" >
   
-          
-          <img src="/images/flash_sale_mob.webp" class="img-fluid banner__mob" alt="seeken" loading="lazy">
-         
+      <NuxtLink :to="`/products/${item.slug}`">
+          <img :src="item.flash_sale_image_url"  class="img-fluid banner__mob" alt="seeken" loading="lazy">
+        </NuxtLink>  
   
           <!-- mobile view -->
-          <a href="#">
+         <NuxtLink :to="`/products/${item.slug}`">
             <div class="sale__box__mobile ">
               <div class="outer__box">
                 <h5>FLASH SALE</h5>
-                <h4>Granite Tawa</h4>
-                <p>Lorem ipsum dolor sit alur. Lorem ipsum dolor sit alur.</p>
+                <h4>{{ item.name }}</h4>
+                <p>{{ item.short_description }}</p>
               </div>
               <div class="bottom_boxes">
                 <div class="limited_offer">LIMITED TIME OFFER</div>
-                <div class="countdown-container" data-endtime="10">
-                  <div class="countdown-box">
-                    <span id="days">25</span>
-                    <span class="label">DAYS</span>
-                  </div>
-                  <div class="countdown-box">
-                    <span id="hours">20</span>
-                    <span class="label">HOURS</span>
-                  </div>
-                  <div class="countdown-box">
-                    <span id="minutes">10</span>
-                    <span class="label">MINUTES</span>
-                  </div>
-                  <div class="countdown-box">
-                    <span id="seconds">5</span>
-                    <span class="label">SECONDS</span>
-                  </div>
-                </div>
-                <h6> <span>10 more</span> items remaining!</h6>
-                <div class="offer">50% OFF</div>
+                <ProductCardCountDown :endTime="item.to_date"/>
+                <h6> <span>{{ item.stock }} more</span> items remaining!</h6>
+                <div class="offer">{{item.discount}}% OFF</div>
               </div>
             </div>
-          </a>
+          </NuxtLink>
         
           <!-- mobile view -->
         </div>
