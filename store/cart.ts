aebef6f554b60f422565e5b5fe0db97e1d6a1ea3
Sherin Reynold <teamlead@phitany.com>
2025-04-07 17:17:18 +0530

    cart store update

--- store/cart.ts
+++ store/cart.ts
@@ -12,11 +12,38 @@ export const useCartStore = defineStore('cart', {
   }),
   actions: {
 
+    async fetchCartFromServer() {
+      const authStore = useAuthStore();
+      let endpoint = `${useRuntimeConfig().public.apiBase}cart`;
+      const { ensureGuestToken } = useGuestToken();
+      const headers: any = {};
+
+      if (authStore.token && authStore.user) {
+        headers['Authorization'] = `Bearer ${authStore.token}`;
+      } else {
+        const guestToken = ensureGuestToken();
+        if (guestToken) headers['X-Guest-Token'] = guestToken;
+        endpoint += `?session_id=${guestToken}`;
+      }
+
+      try {
+        const response = await $fetch(endpoint, {
+          method: 'GET',
+          headers,
+        });
+
+        // Assuming the response is an array of cart items
+        this.cart = response || [];
+      } catch (error) {
+        console.error('Failed to fetch cart:', error);
+      }
+    },
+
     async syncCartWithServer(item) {
 
       const authStore = useAuthStore()
 
-      console.log(item, 'item add to cart')
+      
       const endpoint = `${useRuntimeConfig().public.apiBase}add-to-cart`;
 
       const { ensureGuestToken } = useGuestToken()
@@ -28,7 +55,8 @@ export const useCartStore = defineStore('cart', {
         if (authToken) headers['Authorization'] = `Bearer ${authToken}`
       } else {
         const guestToken = ensureGuestToken()
-        if (guestToken) headers['X-Guest-Token'] = guestToken
+        if (guestToken) headers['X-Guest-Token'] = guestToken;
+        item.session_id = guestToken;
       }
 
       try {
@@ -40,6 +68,9 @@ export const useCartStore = defineStore('cart', {
       } catch (error) {
         console.error('Cart sync failed:', error)
       }
+      finally{
+        this.fetchCartFromServer();
+      }
     },
     addToCart(product) {
       // const existingProduct = this.cart.find(item => item.id === product.id);
@@ -49,7 +80,7 @@ export const useCartStore = defineStore('cart', {
       //   this.cart.push({ ...product, quantity: 1 }); // Add new product with quantity 1
       // }
       let {id }=product;
-      this.syncCartWithServer({id,quantity:1})
+      this.syncCartWithServer({product_id:id,quantity:1,action:'add'})
     },
     removeFromCart(productId) {
       this.cart = this.cart.filter(item => item.id !== productId);
