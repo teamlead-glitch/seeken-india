b1e615aff776200e59feb1df4eda7c044093a4a8
Sherin Reynold <teamlead@phitany.com>
2025-04-24 18:26:21 +0530

    featured mobile view more btn

--- components/Product/HomeFeaturedMob.vue
+++ components/Product/HomeFeaturedMob.vue
@@ -1,38 +1,47 @@
 <template>
 
-<section class="featured__products__mobile">
-  <div class="container">
+  <section class="featured__products__mobile">
+    <div class="container">
 
-    <div class="row align-items-center">
-  <div class="col-md-9 mb-3">
-    <h3>Featured Products</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis aliquet metus, eget ullamcorper nisi
-      egestas vitae.</p>
-  </div>
+      <div class="row align-items-center">
+        <div class="col-md-9 mb-3">
+          <h3>Featured Products</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec mattis aliquet metus, eget ullamcorper nisi
+            egestas vitae.</p>
+        </div>
 
-</div>
-<div class="row mt-4">
-      
-        
-    
-   
+      </div>
+      <div class="row mt-4">
 
 
-     
+
+
+
+
+
         <div class="col-md-12">
           <div class="swiper mySwiper2">
             <div class="swiper-wrapper">
-             
-              <ProductFeaturedMobSlider :slides="products"/>
 
+              <ProductFeaturedMobSlider :slides="products" />
+
+            </div>
+          </div>
+        </div>
       </div>
+
+      <div class="row mt-4">
+       
+       <div class="col-md-12 d-flex justify-content-center">
+        
+         <NuxtLink to="/list" class="btn_2">View More <i class="bi bi-arrow-right"></i></NuxtLink>
+       </div>
+     </div>
+
     </div>
-    </div>
-    </div>
-    </div>
-    
-  
-</section>
+
+
+  </section>
 
 </template>
 
