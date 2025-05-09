0ee1417eca7dfa2178edce906ce4c2b361af83e3
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-09 09:30:39 +0000

    start_server.sh edited online with Bitbucket

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/ubuntu/nuxt-app/seeken-nuxt
-pm2 delete nuxt-app || true
+cd /home/ubuntu/seeken-nuxt
+pm2 delete all
 npm install
 npm run build
 pm2 start .output/server/index.mjs --interpreter=node --name nuxt-app
