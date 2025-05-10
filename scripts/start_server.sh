e7b818ab4187f6016c3802a389893888fb401541
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-10 09:08:57 +0000

    start_server.sh edited online with Bitbucket

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# 🔧 Set ownership
-chown -R ubuntu:ubuntu /home/ubuntu/seeken-nuxt
-
-# 🔁 Run everything as ubuntu user in a login shell
 runuser -l ubuntu -c '
 cd /home/ubuntu/seeken-nuxt
 pm2 delete all || true
@@ -13,5 +9,4 @@ pm2 start .output/server/index.mjs --interpreter=node --name nuxt-app
 pm2 save
 '
 
-# 🔁 Restart nginx
 systemctl restart nginx
