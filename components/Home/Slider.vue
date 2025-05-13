3c9d9cfad357fce31c509f118f4d398ba98e2911
Sherin Reynold <teamlead@phitany.com>
2025-05-13 17:26:50 +0530

    prime products

--- components/Home/Slider.vue
+++ components/Home/Slider.vue
@@ -1,16 +1,16 @@
 <template>
-<section class="products__container" id="section1">
+<section class="products__container" id="section1" v-if="prime_products">
     <div class="container-fluid p-0">
       <div class="row">
         
         
-        <div class="col-md-4 p-0" v-for="(item, index) in products" :key="index">
+        <div class="col-md-4 p-0" v-for="(item, index) in prime_products" :key="index">
           <div class="products__img">
-            <img :src="item.image" class="img-fluid" alt="seeken">
+            <img :src="item.image_path" class="img-fluid" alt="seeken">
             <div class="contents">
-              <h6>{{item.title}}</h6>
-              <h3>{{item.description}}</h3>
-              <a class="btn_1" href="#">Buy </a>
+              <h6>{{item.product_name}}</h6>
+              <h3>{{item.title}}</h3>
+              <NuxtLink class="btn_1" :to="`products/${item.slug}`">Buy </NuxtLink>
             </div>
           </div>
         </div>
@@ -22,12 +22,9 @@
 </template>
 
 <script setup>
-const products = [
-  { title: "New Prod 1", description: "This is the first slide content.", image:"/images/product_1.webp" },
-  { title: "New Prod 2", description: "This is the second slide content.", image:"/images/product_2.webp" },
-  { title: "New Prod 3", description: "This is the third slide content.", image:"/images/product_3.webp" },
-  
-];
+const props = defineProps({
+  prime_products: Array
+});
 
 
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <HomeBanner />
-  <HomeSlider />
+  <HomeSlider :prime_products="prime_products"/>
   <HomeFlashSaleSlider />
   <HomeWhatsNewDesktop :whats_new="whats_new"/>
   <HomeWhatsNewMob :whats_new="whats_new"/>
@@ -15,5 +15,6 @@
 
 
 const { data: whats_new, error, refresh } = useFetchData('whats_new', 'whatsnew');
+const { data: prime_products, error:error1, refresh:refresh2 } = useFetchData('prime_products', 'prime-products');
 
 </script>
