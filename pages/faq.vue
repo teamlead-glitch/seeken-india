91ae0d67b054a9b7790b3c482d36305ac7986980
Sherin Reynold <teamlead@phitany.com>
2025-05-14 16:22:59 +0530

    faq api integration

--- pages/faq.vue
+++ pages/faq.vue
@@ -1,6 +1,7 @@
 <template>
   <CommonInnerBanner page_title="Faq" />
    <section class="inner_container">
+   
         <div class="container">
             <div class="row mb-4">
                 <div class="col-md-12 text-center"><h1>Frequently Asked Questions</h1>
@@ -8,131 +9,42 @@
                </div>
                <div class="row">
                 <div class="col-md-12 terms_service mt-4">
+
+
+
+
+
                     <div class="accordion accordion-flush" id="accordionExample">
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingOne">
-                                <button class="accordion-button" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                    1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis rutrum
-                                    tortor, quis consectetur augue bibendum non. lor
-                                </button>
-                            </h2>
-                            <div id="collapseOne" class="accordion-collapse collapse show"
-                                aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <p>A data structure is defined as a particular way of storing and organizing
-                                        data our devices to use the data efficiently and effectively.The main idea
-                                        behind using data structures is to minimize the time and space complexities.
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus rem veniam
-                                        dignissimos deserunt accusantium sunt iure architecto fugiat saepe sequi
-                                        magnam eaque debitis accusamus, nisi rerum tempora aliquid consequuntur
-                                        explicabo! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                        Quibusdam delectus veritatis incidunt suscipit magnam? Quos qui sint labore
-                                        laudantium ad nostrum perferendis, consequatur nihil architecto! Praesentium
-                                        nemo voluptatum reiciendis facilis!</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingTwo">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                    2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis rutrum
-                                    tortor, quis consectetur augue bibendum non.
-                                </button>
-                            </h2>
-                            <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
-                                data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <p>A data structure is defined as a particular way of storing and organizing
-                                        data our devices to use the data efficiently and effectively.The main idea
-                                        behind using data structures is to minimize the time and space complexities.
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus rem veniam
-                                        dignissimos deserunt accusantium sunt iure architecto fugiat saepe sequi
-                                        magnam eaque debitis accusamus, nisi rerum tempora aliquid consequuntur
-                                        explicabo! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                        Quibusdam delectus veritatis incidunt suscipit magnam? Quos qui sint labore
-                                        laudantium ad nostrum perferendis, consequatur nihil architecto! Praesentium
-                                        nemo voluptatum reiciendis facilis!</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingThree">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseThree" aria-expanded="false"
-                                    aria-controls="collapseThree">
-                                    3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis rutrum
-                                    tortor, quis consectetur augue bibendum non.
-                                </button>
-                            </h2>
-                            <div id="collapseThree" class="accordion-collapse collapse"
-                                aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <p>A data structure is defined as a particular way of storing and organizing
-                                        data our devices to use the data efficiently and effectively.The main idea
-                                        behind using data structures is to minimize the time and space complexities.
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus rem veniam
-                                        dignissimos deserunt accusantium sunt iure architecto fugiat saepe sequi
-                                        magnam eaque debitis accusamus, nisi rerum tempora aliquid consequuntur
-                                        explicabo! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                        Quibusdam delectus veritatis incidunt suscipit magnam? Quos qui sint labore
-                                        laudantium ad nostrum perferendis, consequatur nihil architecto! Praesentium
-                                        nemo voluptatum reiciendis facilis!</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingFour">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseFour" aria-expanded="false"
-                                    aria-controls="collapseFour">
-                                    4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis rutrum
-                                    tortor, quis consectetur augue bibendum non.
-                                </button>
-                            </h2>
-                            <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour"
-                                data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <p>A data structure is defined as a particular way of storing and organizing
-                                        data our devices to use the data efficiently and effectively.The main idea
-                                        behind using data structures is to minimize the time and space complexities.
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus rem veniam
-                                        dignissimos deserunt accusantium sunt iure architecto fugiat saepe sequi
-                                        magnam eaque debitis accusamus, nisi rerum tempora aliquid consequuntur
-                                        explicabo! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                        Quibusdam delectus veritatis incidunt suscipit magnam? Quos qui sint labore
-                                        laudantium ad nostrum perferendis, consequatur nihil architecto! Praesentium
-                                        nemo voluptatum reiciendis facilis!</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="headingFive">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseFive" aria-expanded="false"
-                                    aria-controls="collapseFive">
-                                    5. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis rutrum
-                                    tortor, quis consectetur augue bibendum non.
-                                </button>
-                            </h2>
-                            <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive"
-                                data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <p>A data structure is defined as a particular way of storing and organizing
-                                        data our devices to use the data efficiently and effectively.The main idea
-                                        behind using data structures is to minimize the time and space complexities.
-                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus rem veniam
-                                        dignissimos deserunt accusantium sunt iure architecto fugiat saepe sequi
-                                        magnam eaque debitis accusamus, nisi rerum tempora aliquid consequuntur
-                                        explicabo! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                        Quibusdam delectus veritatis incidunt suscipit magnam? Quos qui sint labore
-                                        laudantium ad nostrum perferendis, consequatur nihil architecto! Praesentium
-                                        nemo voluptatum reiciendis facilis!</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+    <div class="accordion-item" v-for="(faq, index) in faqs" :key="index">
+      <h2 class="accordion-header" :id="`heading${index}`">
+        <button
+          class="accordion-button"
+          :class="{ collapsed: index !== 0 }"
+          type="button"
+          data-bs-toggle="collapse"
+          :data-bs-target="`#collapse${index}`"
+          :aria-expanded="index === 0 ? 'true' : 'false'"
+          :aria-controls="`collapse${index}`"
+        >
+          {{ index + 1 }}. {{ faq.question }}
+        </button>
+      </h2>
+      <div
+        :id="`collapse${index}`"
+        class="accordion-collapse collapse"
+        :class="{ show: index === 0 }"
+        :aria-labelledby="`heading${index}`"
+        data-bs-parent="#accordionExample"
+      >
+        <div class="accordion-body" v-html="faq.answer"></div>
+      </div>
+    </div>
+  </div>
+
+
+
+
+                    
                     </div>
                   </div>
                 </div>
@@ -141,6 +53,8 @@
 </template>
 
 <script lang="ts" setup>
+
+const { data: faqs, error, refresh } = useFetchData('faqs', 'faqs');
 
 useHead({
       title: 'Seeken | Faq',
