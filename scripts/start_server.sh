91917e79addd2880f746b98a9bf22d1e415e3cfc
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-21 09:47:45 +0000

    start_server.sh edited online with Bitbucket

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,9 +4,11 @@
 echo "Running app setup as ubuntu user..."
 sudo runuser -l ubuntu -c '
   cd /home/ubuntu/seeken-nuxt
+  rm -rf node_modules || true
+  rm package-lock.json
   npm install
   npm run build
-  pm2 delete all || true
+  pm2 delete all
   pm2 start npm --name "nuxt-app" -- run start
   pm2 save
 '
