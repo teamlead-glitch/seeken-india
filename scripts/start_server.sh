b9c88fc695a97a5d9ca4ecefc9d5ab121c0999c5
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-21 09:31:25 +0000

    start_server.sh edited online with Bitbucket

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,31 +1,13 @@
 #!/bin/bash
 
-echo "Changing ownership of project files to ubuntu user..."
-chown -R ubuntu:ubuntu /home/ubuntu/seeken-nuxt
-
-echo "Checking owner of the project directory:"
-ls -ld /home/ubuntu/seeken-nuxt
-
-echo "Checking owner of key files:"
-ls -l /home/ubuntu/seeken-nuxt/package.json
-ls -l /home/ubuntu/seeken-nuxt/test.sh
 
 echo "Running app setup as ubuntu user..."
 runuser -l ubuntu -c '
   cd /home/ubuntu/seeken-nuxt
-  echo "Installing dependencies..."
   npm install
-
-  echo "Building the Nuxt app..."
   npm run build
-
-  echo "Stopping any existing PM2 processes..."
   pm2 delete all || true
-
-  echo "Starting Nuxt app with PM2..."
   pm2 start npm --name "nuxt-app" -- run start
-
-  echo "Saving PM2 process list..."
   pm2 save
 '
 
