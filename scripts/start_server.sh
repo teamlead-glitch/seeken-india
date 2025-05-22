d645294882c39b9ec4574f16b17ff71a58d21744
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-22 12:03:21 +0000

    start_server.sh edited online with Bitbucket

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-
 echo "Running app setup as ubuntu user..."
+
 sudo runuser -l ubuntu -c '
   cd /home/ubuntu/seeken-nuxt
-  rm -rf node_modules || true
-  rm package-lock.json
-  npm install
+
+  echo "Cleaning previous node modules and lock file..."
+  rm -rf node_modules package-lock.json
+
+  echo "Installing npm packages with legacy peer deps..."
+  npm install --legacy-peer-deps
+
+  echo "Building Nuxt app..."
   npm run build
-  pm2 delete all
+
+  echo "Restarting Nuxt with PM2..."
+  pm2 delete all || true
   pm2 start npm --name "nuxt-app" -- run start
   pm2 save
 '
