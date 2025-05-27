6d409f557abd7412801ff27f57ea4fffda997841
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-27 12:16:34 +0000

    start_server.sh edited online with Bitbucket

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,11 +3,11 @@
 # Ensure the directory and contents are owned by ubuntu
 sudo chown -R ubuntu:ubuntu /home/ubuntu/seeken-nuxt
 
-# Run installation, build, and restart as ubuntu
-sudo -u ubuntu bash -c '
+# Run installation, build, and start as ubuntu
+sudo -u ubuntu bash -c "
   cd /home/ubuntu/seeken-nuxt &&
-  rm -rf node_modules .nuxt dist &&
   npm install &&
   npm run build &&
-  pm2 restart nuxt-app --update-env
-'
+  pm2 stop nuxt-app || true &&
+  pm2 start nuxt-app --update-env
+"
