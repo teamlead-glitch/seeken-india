cfbf71f39170c724a9bbde69a8a1e13e1870e84d
Sherin Reynold <teamlead@phitany.com>
2025-05-27 14:31:34 +0530

    variant initial reset issue fix

--- components/Product/Card/VarientsChoose.vue
+++ components/Product/Card/VarientsChoose.vue
@@ -7,7 +7,7 @@
                                 <ul>
                                   <!-- {{ defaultOptionIdArray }} -->
                                     <li v-if="options?.product_option_values?.length > 0" v-for="option_values in options.product_option_values">
-                                        <div style="width: auto; padding: 10px;" class="code" :class="{ active: selectedOptions[options.id] === option_values.id || defaultOptionIdArray.includes(option_values.id)}" @click="chooseVariant(options.id, option_values.id)"> 
+                                        <div style="width: auto; padding: 13px;border-radius: 8%" class="code" :class="{ active: selectedOptions[options.id] === option_values.id || defaultOptionIdArray.includes(option_values.id)}" @click="chooseVariant(options.id, option_values.id)"> 
                                           <!-- <img src="/images/color_code/clr-1.webp" class="img-fluid" alt="seeken" loading="lazy"> -->
                                         {{ option_values.value }}
                                         </div>
@@ -54,4 +54,15 @@ console.log(defaultOptionIdArray,'defaultOptionIdArray++')
    
   }
 });
+
+watch(() => props.product, (newProduct) => {
+  if (!newProduct?.product_options) return;
+
+  newProduct.product_options.forEach(option => {
+    if (option.product_option_values?.length > 0) {
+      // Optionally use defaultOptionIdArray here if needed
+      selectedOptions[option.id] = option.product_option_values[0].id;
+    }
+  });
+}, { immediate: true });
   </script>
