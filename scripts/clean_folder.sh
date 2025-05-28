54c43a241963df3b9088964c001f11def16d5732
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-28 16:22:59 +0000

    clean_folder.sh edited online with Bitbucket

--- scripts/clean_folder.sh
+++ scripts/clean_folder.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-sudo -u ubuntu bash -c "rm -rf /home/ubuntu/seeken-nuxt/*"
-sudo -u ubuntu bash -c "rm -rf /home/ubuntu/seeken-nuxt/."
 
+echo "Cleaning up previous deployment..."
+
+rm -rf /opt/seeken-nuxt/*
+chown -R ssm-user:ssm-user /opt/seeken-nuxt
