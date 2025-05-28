ef9d1378dd8b28f6534bbe98cf27f9fa6f4c5e38
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-28 16:23:40 +0000

    install_dependencies.sh edited online with Bitbucket

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sudo chown -R ubuntu:ubuntu /home/ubuntu/seeken-nuxt
 
+cd /opt/seeken-nuxt || exit 1
 
+echo "Installing npm dependencies..."
+npm install
 
+echo "Building Nuxt SSR app..."
+npm run build
