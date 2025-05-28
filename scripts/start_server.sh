f1b2e6427f452b91e9a8e1a2e09cae50a01a9895
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-28 06:56:55 +0000

    start_server.sh edited online with Bitbucket

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
-LOGFILE="/var/log/seeken_deploy.log"
+sudo chown -R ubuntu:ubuntu /home/ubuntu/seeken-nuxt 
 
-echo "=== Deployment started at $(date) ===" >> $LOGFILE 2>&1
-
-echo "Killing PM2 daemons for ssm-user and root..." >> $LOGFILE 2>&1
-sudo pkill -u ssm-user pm2 >> $LOGFILE 2>&1 || true
-sudo pkill -u root pm2 >> $LOGFILE 2>&1 || true
-
-echo "Changing ownership of /home/ubuntu/seeken-nuxt ..." >> $LOGFILE 2>&1
-sudo chown -R ubuntu:ubuntu /home/ubuntu/seeken-nuxt >> $LOGFILE 2>&1
-
-echo "Running npm install, build and pm2 restart as ubuntu user..." >> $LOGFILE 2>&1
 sudo -u ubuntu bash -c '
   cd /home/ubuntu/seeken-nuxt &&
-  npm install >> '"$LOGFILE"' 2>&1 &&
-  npm run build >> '"$LOGFILE"' 2>&1 &&
-  pm2 restart nuxt-app --update-env >> '"$LOGFILE"' 2>&1
+  npm install  &&
+  npm run build &&
+  pm2 restart nuxt-app --update-env 
 '
-
-echo "=== Deployment finished at $(date) ===" >> $LOGFILE 2>&1
