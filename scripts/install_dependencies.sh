ce7e1c3ed28c5244a6fb70dfecd6bc731efadf89
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-28 18:38:56 +0000

    install_dependencies.sh edited online with Bitbucket

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-cd /opt/seeken-nuxt || exit 1
-
-sudo chown -R ssm-user:ssm-user .
-
-npm install >> /tmp/deploy.log 2>&1
-npm run build >> /tmp/deploy.log 2>&1
-
-echo "Deploy finished at $(date)" >> /tmp/deploy.log
+cd /opt/seeken-nuxt && \
+sudo chown -R ssm-user:ssm-user . && \
+npm install && \
+npm run build > /tmp/deploy.log 2>&1
