e94a2aa2810ff028e1af2b51fa3138b242d374b8
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-29 10:05:23 +0000

    install_dependencies.sh edited online with Bitbucket

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf /opt/seeken-nuxt
-mkdir /opt/seeken-nuxt
-unzip /opt/seeken-nuxt-tmp/build_output.zip -d /opt/seeken-nuxt
-cd /opt/seeken-nuxt && npm install && npm run build 
+sudo rm -rf /opt/seeken-nuxt
+sudo mkdir /opt/seeken-nuxt
+sudo unzip /opt/seeken-nuxt-tmp/build_output.zip -d /opt/seeken-nuxt
+sudo cd /opt/seeken-nuxt && npm install && npm run build 
 sudo systemctl restart nuxt-app.service
