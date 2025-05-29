877ddbf85ce4eabe11ed36382a7192949ec8c221
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-29 10:34:45 +0000

    test.sh edited online with Bitbucket

--- test.sh
+++ test.sh
@@ -2,7 +2,6 @@
 sudo rm -rf /opt/seeken-nuxt
 sudo mkdir /opt/seeken-nuxt
 sudo unzip /opt/seeken-nuxt-tmp/build_output.zip -d /opt/seeken-nuxt
-sudo cd /opt/seeken-nuxt && npm install && npm run build 
-npm install && npm run build
+cd /opt/seeken-nuxt && sudo npm install && sudo npm run build 
 sudo systemctl restart nuxt-app.service
 
