08f52b19d003249c652f7f22666d8190f10a9b22
Sangeeth Mohan <sangeezvk18@gmail.com>
2025-05-29 10:18:00 +0000

    test.sh edited online with Bitbucket

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-# Dummy script for AWS CodeDeploy post-deploy hook
-
-echo "Dummy test.sh executed. No actual checks performed."
+sudo rm -rf /opt/seeken-nuxt
+sudo mkdir /opt/seeken-nuxt
+sudo unzip /opt/seeken-nuxt-tmp/build_output.zip -d /opt/seeken-nuxt
+sudo cd /opt/seeken-nuxt && npm install && npm run build 
+sudo systemctl restart nuxt-app.service
