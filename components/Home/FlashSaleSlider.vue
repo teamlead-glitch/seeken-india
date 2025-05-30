200b35a52e5b358e4ae83ab48e9464cbf74e3278
Sherin Reynold <teamlead@phitany.com>
2025-05-30 12:55:29 +0530

    flashsale mob image

--- components/Home/FlashSaleSlider.vue
+++ components/Home/FlashSaleSlider.vue
@@ -60,7 +60,7 @@
     <div class="mobile__parent__div" :class="{ outOfStock: !item.stock || item.stock < 1 }">
   
       <NuxtLink :to="`/products/${item.slug}`">
-          <img :src="item.flash_sale_image_url"  class="img-fluid banner__mob" alt="seeken" loading="lazy">
+          <img :src="item.flash_sale_image_mobile_url"  class="img-fluid banner__mob" alt="seeken" loading="lazy">
         </NuxtLink>  
   
           <!-- mobile view -->
