d705f1ab82453ddec1739192dae97736c3ce03b9
Sherin Reynold <teamlead@phitany.com>
2025-06-04 14:09:56 +0530

    variant available check on featured products

--- components/Home/FeaturedProducts.vue
+++ components/Home/FeaturedProducts.vue
@@ -1,12 +1,40 @@
 <template>
-    <ProductHomeFeaturedDesk :products="featured_products"/>
+    <ProductHomeFeaturedDesk :products="checkVariantAvailable(featured_products)"/>
 
-    <ProductHomeFeaturedMob :products="featured_products"/>
+    <ProductHomeFeaturedMob :products="checkVariantAvailable(featured_products)"/>
 </template>
 
 
 <script setup>
 
 const { data: featured_products, error, refresh } = useFetchDataCache('featured_products', 'featured-products');
+
+const getModifiedProduct = (item) => {
+  if (item.stock_quantity > 0) return item;
+
+  const availableVariant = item.product_variants?.find(
+    v => v.variant_price?.stock_quantity > 0
+  );
+
+  if (availableVariant) {
+    return {
+      ...item,
+      stock_quantity: availableVariant.variant_price.stock_quantity,
+      final_price: availableVariant.variant_price.final_price,
+      price: availableVariant.variant_price.price,
+      selling_price: availableVariant.variant_price.selling_price,
+      ...(availableVariant.variant_image_path
+        ? { image_path: availableVariant.variant_image_path }
+        : {}),
+    };
+  }
+
+  return item;
+};
+
+// Apply getModifiedProduct to each product
+const checkVariantAvailable = (products) => {
+  return products?.map(getModifiedProduct) ?? [];
+};
 
 </script>
