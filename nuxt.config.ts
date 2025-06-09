517c2db6131f2d505e22638d9cf50b786101562f
Sherin Reynold <teamlead@phitany.com>
2025-06-09 12:15:25 +0530

    pwa icon updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -33,16 +33,16 @@ export default defineNuxtConfig({
       description: 'A Nuxt 3 Progressive Web App example',
       theme_color: '#ffffff',
       icons: [
-        {
-          src: '/icon-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
+        { src: '/images/app-logos/bg/48x48.png',   sizes: '48x48',   type: 'image/png' },
+      { src: '/images/app-logos/bg/72x72.png',   sizes: '72x72',   type: 'image/png' },
+      { src: '/images/app-logos/bg/96x96.png',   sizes: '96x96',   type: 'image/png' },
+      { src: '/images/app-logos/bg/128x128.png', sizes: '128x128', type: 'image/png' },
+      { src: '/images/app-logos/bg/144x144.png', sizes: '144x144', type: 'image/png' },
+      { src: '/images/app-logos/bg/152x152.png', sizes: '152x152', type: 'image/png' },
+      { src: '/images/app-logos/bg/192x192.png', sizes: '192x192', type: 'image/png' },
+      { src: '/images/app-logos/bg/256x256.png', sizes: '256x256', type: 'image/png' },
+      { src: '/images/app-logos/bg/384x384.png', sizes: '384x384', type: 'image/png' },
+      { src: '/images/app-logos/bg/512x512.png', sizes: '512x512', type: 'image/png' },
       ],
     },
     // Workbox settings to manage caching
