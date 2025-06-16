a46b40c2f839c7f713d3d4eef818a8dd92222f1e
Sherin Reynold <teamlead@phitany.com>
2025-06-16 21:45:22 +0530

    grab mobile update

--- components/Layouts/MobileMenu.vue
+++ components/Layouts/MobileMenu.vue
@@ -26,9 +26,9 @@
         <i class="bi bi-chevron-down" @click="grab=!grab"></i>
       </div>
       <NuxtLink :to="`/sales`" style="color: white !important;">
-      <h6>Grab your Flash Deals Now</h6>
+      <h6>Grab your Flash Deals Now </h6>
       </NuxtLink>
-      <p :style="{ display: grab ? 'block' : 'none' }">
+      <p :style="{ display: grab === true ? 'block' : 'none' }">
         <NuxtLink :to="`/sales`" style="color: white !important;">
 We have a flash sale running with 40-70% off for a limited period and stock.
         </NuxtLink></p>
