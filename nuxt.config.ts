67798024c6bbf47b9291da72d5a394871d882312
Sherin Reynold <teamlead@phitany.com>
2025-06-19 12:11:13 +0530

    config update for home reload issue fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -257,7 +257,7 @@ export default defineNuxtConfig({
     // }
 
     prerender: {
-      routes: ["/",'/blog'],       // Only prerender the index page
+      routes: ['/blog'],       // Only prerender the index page
       crawlLinks: false,   // Prevent crawling links to auto-discover routes
       ignore: ["/b"], // Ignore invalid route
     }
